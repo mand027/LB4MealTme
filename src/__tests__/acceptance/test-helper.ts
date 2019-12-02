a792367c354790a46a8c733f7cfa65f8d9fc4746
KevinRuvalcaba <kevinalejandro228@hotmail.com>
2019-12-01 20:33:17 -0600

    clean shit

---
+++ src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {MealtimelbApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new MealtimelbApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: MealtimelbApplication;
+  client: Client;
+}
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {MealtimelbApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {MealtimelbApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new MealtimelbApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
